512aa95c0e73cad6e355f33a28738b37d82f38f1
huangyulu <huangyulu@eperfect.com.cn>
2026-01-26 14:34:08 +0800

    MA-000: init project

---
+++ src/main/java/com/minilab/Main.java
@@ -0,0 +1,13 @@
+package com.minilab;
+
+/**
+ * @Author huangyulu
+ * @Date 2026/1/26 14:31
+ * @Description
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
